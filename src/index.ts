3495926894a328e714159d6ef8af7917a73487ae
clo3olb <clo3olb@gmail.com>
2023-10-04 15:31:17 +0800

    main testing script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-const BASE_URL = "https://query1.finance.yahoo.com";
-const TICKER = "AAPL";
-const REGION = "US";
-const corsDomain = "www.hello.com";
-const API_URL = `${BASE_URL}/v8/finance/chart/${TICKER}?region=${REGION}&lang=en-US&includePrePost=false&interval=2m&useYfid=true&range=1d&corsDomain=${corsDomain}&.tsrc=finance`;
+import { getYahooQuotes } from "./api/yahoo";
 
-console.log(API_URL);
+async function main() {
+  const quotes = await getYahooQuotes("AAPL");
+  console.log({ quotes });
+}
+
+main();
