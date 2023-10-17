2c15966ccaeee720fcc01bda952c36b79347ce55
clo3olb <clo3olb@gmail.com>
2023-10-17 19:50:40 +0800

    add date format functions

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,4 +1,9 @@
-import { createUrlWithParams } from "./util";
+import {
+  createUrlWithParams,
+  formatDate,
+  formatFiscalEndingDate,
+  monthToNum,
+} from "./util";
 
 describe("createUrlWithParams", () => {
   it("must correctly parse params into url", () => {
@@ -33,5 +38,59 @@ describe("createUrlWithParams", () => {
       { w: undefined, h: undefined }
     );
     expect(url).toBe("https://www.google.com?crop=entropy&q=85");
+  });
+});
+
+describe("formatFiscalEndingDate", () => {
+  it("must format Jan/2023 to 2023-01-31", () => {
+    const date = formatFiscalEndingDate("Jan/2023");
+    expect(date).toBe("2023-01-31");
+  });
+  it("must format Feb/2023 to 2023-02-28", () => {
+    const date = formatFiscalEndingDate("Feb/2023");
+    expect(date).toBe("2023-02-28");
+  });
+  it("must format Feb/2016 to ", () => {
+    const date = formatFiscalEndingDate("Feb/2016");
+    expect(date).toBe("2016-02-29");
+  });
+  it("must format Mar/2023 to 2023-03-31", () => {
+    const date = formatFiscalEndingDate("Mar/2023");
+    expect(date).toBe("2023-03-31");
+  });
+  it("must format Dec/2023 to 2023-12-31", () => {
+    const date = formatFiscalEndingDate("Dec/2023");
+    expect(date).toBe("2023-12-31");
+  });
+});
+
+describe("formatDate", () => {
+  it("must format 2021-01-01 to 2021-01-01", () => {
+    const date = new Date("2021-01-01");
+    const formattedDate = formatDate(date);
+    expect(formattedDate).toBe("2021-01-01");
+  });
+  it("must format 2021/01/02 to 2021/01/02", () => {
+    const date = new Date("2021/01/02");
+    const formattedDate = formatDate(date);
+    expect(formattedDate).toBe("2021-01-02");
+  });
+});
+
+describe("monthToNum", () => {
+  it("must return 1 for jan", () => {
+    const n = monthToNum("jan");
+    expect(n).toBe(1);
+  });
+  it("must return 2 for feb", () => {
+    const n = monthToNum("feb");
+    expect(n).toBe(2);
+  });
+  it("must return 12 for december", () => {
+    const n = monthToNum("december");
+    expect(n).toBe(12);
+  });
+  it("must have error for unexpected string", () => {
+    expect(() => monthToNum("hello")).toThrow();
   });
 });
--- src/util.ts
+++ src/util.ts
@@ -35,3 +35,70 @@ export function createUrlWithParams(
     paramsList.join("&")
   );
 }
+
+export function formatFiscalEndingDate(date: string) {
+  // Aug/2023 => 2023-08-31
+  const month = date.split("/")[0];
+  const year = date.split("/")[1];
+
+  const monthNum = monthToNum(month);
+  const day = new Date(Number(year), monthNum, 0).getDate();
+
+  const monthStr = monthNum < 10 ? "0" + monthNum : monthNum;
+  const dayStr = day < 10 ? "0" + day : day;
+
+  return `${year}-${monthStr}-${dayStr}`;
+}
+
+export function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  const monthStr = month < 10 ? "0" + month : month;
+  const dayStr = day < 10 ? "0" + day : day;
+
+  return `${year}-${monthStr}-${dayStr}`;
+}
+
+export function monthToNum(month: string) {
+  switch (month.toLowerCase()) {
+    case "jan":
+    case "january":
+      return 1;
+    case "feb":
+    case "february":
+      return 2;
+    case "mar":
+    case "march":
+      return 3;
+    case "apr":
+    case "april":
+      return 4;
+    case "may":
+      return 5;
+    case "jun":
+    case "june":
+      return 6;
+    case "jul":
+    case "july":
+      return 7;
+    case "aug":
+    case "august":
+      return 8;
+    case "sep":
+    case "september":
+      return 9;
+    case "oct":
+    case "october":
+      return 10;
+    case "nov":
+    case "november":
+      return 11;
+    case "dec":
+    case "december":
+      return 12;
+    default:
+      throw new Error("Invalid month");
+  }
+}
