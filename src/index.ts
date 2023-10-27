ff49e185ae2ea6399b6ffd998d9603b17af0ce03
clo3olb <clo3olb@gmail.com>
2023-10-27 18:19:01 +0800

    add progress bar

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 import { getNasdaqIntradayQuotes } from "./api/nasdaq";
 import { getWikipediaSP500CompaniesList } from "./api/wikipedia";
 import { saveIntradayQuotes } from "./save";
+import progress from "cli-progress";
 
 async function main() {
   const sp500List = await getWikipediaSP500CompaniesList();
+  const bar = new progress.SingleBar(
+    {
+      format:
+        "Infraday [{bar}] {percentage}% | {company} | ETA: {eta}s | {value}/{total}",
+    },
+    progress.Presets.shades_classic
+  );
+  bar.start(sp500List.length, 0);
 
   // Save Intraday Quotes
   for (let i = 0; i < sp500List.length; i++) {
+    bar.update(i, { company: sp500List[i].symbol + " - " + sp500List[i].name });
     const company = sp500List[i];
-    console.log(`Fetching ${company.symbol}...`);
     const quotes = await getNasdaqIntradayQuotes(company.symbol);
     saveIntradayQuotes(company.symbol, quotes);
-    console.log(`Saved ${company.symbol}`);
+    bar.update(i + 1);
   }
+  bar.stop();
 }
 
 main();
