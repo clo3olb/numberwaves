65902802c2a5a87e99dc582ad9ee1e5455d279ef
clo3olb <clo3olb@gmail.com>
2023-10-27 18:07:07 +0800

    change index to run the program

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-import { getYahooQuotes } from "./api/yahoo";
+import { getNasdaqIntradayQuotes } from "./api/nasdaq";
+import { getWikipediaSP500CompaniesList } from "./api/wikipedia";
+import { saveIntradayQuotes } from "./save";
 
 async function main() {
-  const quotes = await getYahooQuotes("AAPL");
-  console.log({ quotes });
+  const sp500List = await getWikipediaSP500CompaniesList();
+
+  // Save Intraday Quotes
+  for (let i = 0; i < sp500List.length; i++) {
+    const company = sp500List[i];
+    console.log(`Fetching ${company.symbol}...`);
+    const quotes = await getNasdaqIntradayQuotes(company.symbol);
+    saveIntradayQuotes(company.symbol, quotes);
+    console.log(`Saved ${company.symbol}`);
+  }
 }
 
 main();
